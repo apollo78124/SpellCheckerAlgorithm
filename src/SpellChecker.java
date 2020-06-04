3e2edab0016e0b42f9661742e3f0ed509afe7c3f
apollo78124 <98apollo@naver.com>
2020-06-03 20:26:05 -0700

    Binary search implemented and timer implemented

--- src/SpellChecker.java
+++ src/SpellChecker.java
@@ -16,7 +16,7 @@ public class SpellChecker {
 		 *  	
 		 */
 		for(int i = 0; i < wordlist.length; i++) {
-			if(wordlist[i].equals(word))
+			if(word.equals(wordlist[i]))
 					return true;
 		}
 		
@@ -36,16 +36,26 @@ public class SpellChecker {
 			* else l = m+1
 		* return -1
 		*/
-			
-			
-			
+		int l = 0;
+		int r = wordlist.length-1;
+		int m = 0;
+		while (l <= r) {
+			m = (int) (Math.floor(l+r)/2);
+			if (word.equals(wordlist[m])) {
+				return true;
+			} else if(word.compareToIgnoreCase(wordlist[m]) < 0) {
+				r = m-1;
+			} else {
+				l = m+1;
+			}
+		}
 		return false;
 	}
 	
 	public static void main(String[] args) {
 		Scanner sc = null;
 		try {
-			sc = new Scanner(new File("lab4_wordlist_small.txt"));
+			sc = new Scanner(new File("lab4_wordlist.txt"));
 		} catch (FileNotFoundException ee) {
 			// TODO Auto-generated catch block
 			ee.printStackTrace();
@@ -56,10 +66,46 @@ public class SpellChecker {
 		}
 		String[] dictionary = lines.toArray(new String[0]);
 		
+		try {
+			sc = new Scanner(new File(args[0]));
+		} catch (FileNotFoundException ee) {
+			// TODO Auto-generated catch block
+			ee.printStackTrace();
+		}
+		ArrayList<String> words = new ArrayList<String>();
+		while (sc.hasNext()) {
+			words.add(sc.next());
+		}
+		String[] paragraph = words.toArray(new String[0]);
+		
 		SpellChecker spellChecker = new SpellChecker();
 		
-		System.out.println(spellChecker.SeqSearch(dictionary, "hi"));
+		int seError = 0;
+		int binaryError = 0;
+		long seqTime = 0;
+		long binTime = 0;
+		Stopwatch stopwatch = new Stopwatch();
 		
-		System.out.println(spellChecker.BinSearch(dictionary, "hi"));
+		
+		stopwatch.start();
+		for(int j = 0; j < paragraph.length; j++) {
+			if (!spellChecker.SeqSearch(dictionary, paragraph[j]))
+				seError++;
+		}
+		stopwatch.stop();
+		seqTime = stopwatch.getElapsedTime();
+		
+		stopwatch.reset();
+		
+		stopwatch.start();
+		for(int j = 0; j < paragraph.length; j++) {
+			if (!spellChecker.BinSearch(dictionary, paragraph[j]))
+				binaryError++;
+		}
+		stopwatch.stop();
+		binTime = stopwatch.getElapsedTime();
+		
+		System.out.println("Sequential Search: " + seError + " errors, " + seqTime + " milliseconds.");
+		System.out.println("Binary Search: " + binaryError + " errors, " + binTime + " milliseconds.");
 	}
 }
